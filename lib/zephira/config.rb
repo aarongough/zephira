d8afb705692b31973ff47513cfbd483779f3baf5
Aaron Gough (He/Him) <aaron@goughcustom.com>
2026-04-18 12:48:49 -0400

    Add missing spec cases and refactor `Config`

--- lib/zephira/config.rb
+++ lib/zephira/config.rb
@@ -3,21 +3,13 @@
 module Zephira
   class Config
     def self.read(key)
-      return ENV[key] if ENV.key?(key)
-
       project_config_path = File.join(Dir.pwd, ".zephira.yml")
-      if File.exist?(project_config_path)
-        project_config = YAML.load_file(project_config_path)
-        return project_config[key] if project_config.key?(key)
-      end
-
       global_config_path = File.expand_path("~/.zephira.yml")
-      if File.exist?(global_config_path)
-        global_config = YAML.load_file(global_config_path)
-        return global_config[key] if global_config.key?(key)
-      end
 
-      nil
+      project_config = File.exist?(project_config_path) ? YAML.load_file(project_config_path)[key] : nil
+      global_config = File.exist?(global_config_path) ? YAML.load_file(global_config_path)[key] : nil
+
+      ENV[key] || project_config || global_config
     end
   end
 end
--- spec/zephira/config_spec.rb
+++ spec/zephira/config_spec.rb
@@ -11,17 +11,17 @@ RSpec.describe Zephira::Config do
       FakeFS.activate!
     end
 
-    after do      
+    after do
       FakeFS.deactivate!
     end
 
     def create_project_config(key: "ZEPHIRA_TEST_KEY", value: "project_key")
-      File.write(".zephira.yml", "#{key}: #{value}", mode: "a")
+      File.write(".zephira.yml", "#{key}: #{value}\n", mode: "a")
     end
 
     def create_global_config(key: "ZEPHIRA_TEST_KEY", value: "global_key")
       FileUtils.mkdir_p(File.expand_path("~"))
-      File.write(File.expand_path("~/.zephira.yml"), "#{key}: #{value}", mode: "a")
+      File.write(File.expand_path("~/.zephira.yml"), "#{key}: #{value}\n", mode: "a")
     end
 
     context "when only the environment variable is set" do
@@ -52,6 +52,15 @@ RSpec.describe Zephira::Config do
       end
     end
 
+    context "when the project config exists but does not contain the key and the global config does" do
+      it "returns the value from the global config file" do
+        create_project_config(key: "OTHER_KEY")
+        create_global_config
+
+        expect(described_class.read("ZEPHIRA_TEST_KEY")).to eq("global_key")
+      end
+    end
+
     context "when the global config exists but does not contain the key" do
       it "returns nil" do
         create_global_config(key: "OTHER_KEY")
@@ -75,6 +84,16 @@ RSpec.describe Zephira::Config do
         create_project_config
 
         expect(described_class.read("ZEPHIRA_TEST_KEY")).to eq("env_test_key")
+      end
+    end
+
+    context "when the environment variable is set to an empty string" do
+      it "returns the empty string (and does not fall back to config files)" do
+        ENV["ZEPHIRA_TEST_KEY"] = ""
+        create_global_config
+        create_project_config
+
+        expect(described_class.read("ZEPHIRA_TEST_KEY")).to eq("")
       end
     end
 
